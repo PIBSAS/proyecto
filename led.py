4e200fc65e8a78e3f12926d55342ace4119201e5
PIBSAS <lucianorabassa@gmail.com>
2024-12-24 19:30:31 +0000

    Ahí arregle el Pin que era el 5 y el comentario del sleep

--- led.py
+++ led.py
@@ -2,10 +2,10 @@ from machine import Pin
 import time
 
 # Configurar el pin 8 como salida
-led = Pin(8, Pin.OUT)
+led = Pin(5, Pin.OUT)
 
 while True:
     led.value(1)  # Encender el LED
     time.sleep(3) # Esperar 3 segundos
     led.value(0)  # Apagar el LED
-    time.sleep(6) # Esperar 5 segundos
+    time.sleep(6) # Esperar 6 segundos
