663de1ef6454966cea8d636c8ddda4bbb51cfecb
PIBSAS <lu@lu>
2024-12-24 16:15:51 -0300

    Subiendo desde Pi de Casa

---
+++ led.py
@@ -0,0 +1,11 @@
+from machine import Pin
+import time
+
+# Configurar el pin 8 como salida
+led = Pin(8, Pin.OUT)
+
+while True:
+    led.value(1)  # Encender el LED
+    time.sleep(3) # Esperar 3 segundos
+    led.value(0)  # Apagar el LED
+    time.sleep(5) # Esperar 5 segundos
