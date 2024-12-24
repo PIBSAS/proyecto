96a00cdb8cc8fd78318698389f873a5599cd8482
PIBSAS <lu@lu>
2024-12-24 16:19:10 -0300

    Subiendo desde Pi de Casa v2

--- led.py
+++ led.py
@@ -8,4 +8,4 @@ while True:
     led.value(1)  # Encender el LED
     time.sleep(3) # Esperar 3 segundos
     led.value(0)  # Apagar el LED
-    time.sleep(5) # Esperar 5 segundos
+    time.sleep(6) # Esperar 5 segundos
